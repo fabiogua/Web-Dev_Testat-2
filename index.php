18f8f792126a389150eda8c01930e20440be82ee
misomome <102870237+misomome@users.noreply.github.com>
2022-04-29 22:03:58 +0200

    Armin stinkt

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/src/');
+	header('Location: '.$uri.'/Web-Dev_Testat-2/src/');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
